aa03cdd71d77a83d8d4d21346864dbc5659df1b0
gqq <guoqinqin@xiaoyou.com>
2017-01-01 12:04:54 +0800

    主页面搭建，视频播放完成

--- app/src/main/java/com/feicuiedu/hunttreasure/MainActivity.java
+++ app/src/main/java/com/feicuiedu/hunttreasure/MainActivity.java
@@ -1,13 +1,39 @@
 package com.feicuiedu.hunttreasure;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+import butterknife.Unbinder;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Unbinder mBind;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mBind = ButterKnife.bind(this);
+    }
+
+    // 跳转
+    @OnClick({R.id.btn_Register, R.id.btn_Login})
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.btn_Register:
+
+                break;
+            case R.id.btn_Login:
+                break;
+        }
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        mBind.unbind();
     }
 }
---
+++ app/src/main/java/com/feicuiedu/hunttreasure/user/LoginActivity.java
@@ -0,0 +1,15 @@
+package com.feicuiedu.hunttreasure.user;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.feicuiedu.hunttreasure.R;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
